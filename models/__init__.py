3da12a116586843295046fe5cee8e60d9b35b9b9
Festus1914 <oladiplentyfestus@gmail.com>
2023-06-21 09:08:05 +0000

    Updating the fle

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-"""__init__ is the  magic method for models directory"""
-from models.engine.file_storage import FileStorage
+"""
+__init__ is the  magic method for models directory
+"""
+# from models.base_model import BaseModel, Base
+from os import getenv
 
 
-storage = FileStorage()
+is_type = getenv("HBNB_TYPE_STORAGE")
+
+if is_type == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+import models
+from models.city import City
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import getenv
 
 
-class State(BaseModel):
-    """Represent a state.
+class State(BaseModel, Base):
+    """ State class """
+    if models.is_type == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
+    else:
+        name = ""
 
-    Attributes:
-        name (str): The name of the state.
-    """
-
-    name = ""
+    if models.is_type != 'db':
+        @property
+        def cities(self):
+            cities_list = []
+            all_cities = models.storage.all(City).values()
+            for city in all_cities:
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
