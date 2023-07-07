ebd03df3e7949b123b1d723128677f167be44f32
Festus1914 <oladiplentyfestus@gmail.com>
2023-07-06 21:06:11 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
-"""
-Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers, using the function do_deploy
-"""
-from os import path
-from fabric.api import env, put, run
+"""Fabric script that generates a .tgz archive from the contents of the
+ web_static folder of your AirBnB Clone repo, using the function do_pack."""
 
-env.hosts = ["34.231.110.206", "3.239.57.196"]
+from fabric.api import *
+from datetime import datetime
+import os
+
+env.hosts = ['34.74.27.162', '54.224.88.154']
+
+
+def do_pack():
+    try:
+        formato = "%Y%m%dT%H%M%S"
+        date_now = datetime.now()
+        created_at = date_now.created_at.strftime(formato)
+        local("mkdir -p /versions")
+        file_tgz = "web_static_{}.tgz".format(created_at)
+        local("tar -cvzf versions/{}.tgz web_static".format(file_tgz))
+        return file_tgz
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """
-    Distributes archives to web servers
-    """
-    if not path.exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
-    compressedFile = archive_path.split("/")[-1]
-    fileName = compressedFile.split(".")[0]
-    upload_path = "/tmp/{}".format(compressedFile)
-    if put(archive_path, upload_path).failed:
+    try:
+        put(archive_path, "/tmp")
+        id_file = archive_path.split("_")
+        id_final = id_file[2][:-4]
+        folder = "/data/web_static/releases/"
+        run("mkdir -p {}web_static_{}/".format(folder, id_final))
+        run("tar -xzf /tmp/web_static_{}.tgz -C {}web_static_{}/"
+            .format(id_final, folder, id_final))
+        run("rm /tmp/web_static_{}.tgz".format(id_final))
+        run("mv {}web_static_{}/web_static/* {}web_static_{}/"
+            .format(folder, id_final, folder, id_final))
+        run("rm -rf {}web_static_{}/web_static".format(folder, id_final))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}web_static_{}/ /data/web_static/current"
+            .format(folder, id_final))
+        return True
+    except:
         return False
-    current_release = '/data/web_static/releases/{}'.format(fileName)
-    if run("rm -rf {}".format(current_release)).failed:
-        return False
-    if run("mkdir -p {}".format(current_release)).failed:
-        return False
-    uncompress = "tar -xzf /tmp/{} -C {}".format(
-        compressedFile, current_release
-    )
-    if run(uncompress).failed:
-        return False
-    delete_archive = "rm -f /tmp/{}".format(compressedFile)
-    if run(delete_archive).failed:
-        return False
-    if run("rm -rf /data/web_static/current").failed:
-        return False
-    relink = "ln -s {} /data/web_static/current".format(current_release)
-    if run(relink).failed:
-        return False
-    return True
